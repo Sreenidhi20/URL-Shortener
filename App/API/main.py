2f2f8265991844bc91d9d6c4609d5b6e91920988
Sreenidhi <sreenidhibs04@gmail.com>
2026-08-02 15:16:48 +0530

    feat: add initial FastAPI folder structure and health check endpoint.

---
+++ App/API/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="URL Shortener API",
+    version="1.0.0"
+)
+
+@app.get("/")
+def root():
+    return {"message": "Welcome to the URL Shortener API!"}
---
+++ App/Readme.md
@@ -0,0 +1,1 @@
+Backend logic to shorten the URL.
