a44963efa5cc235c404f7e68319128960025b4c1
Sreenidhi <sreenidhibs04@gmail.com>
2026-08-02 14:36:47 +0530

    feat: initialize React application with TypeScript and Vite
    - Added main entry point for React application (main.tsx).
    - Created TypeScript configuration for the application (tsconfig.app.json).
    - Set up project references in the root TypeScript configuration (tsconfig.json).
    - Added Node-specific TypeScript configuration (tsconfig.node.json).
    - Configured Vite for React application (vite.config.ts).

---
+++ Client/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h2>Welcome to URL shortener App</h2>
+      <p>Please wait while we are building our application</p>
+    </>
+  );
+}
+
+export default App;
